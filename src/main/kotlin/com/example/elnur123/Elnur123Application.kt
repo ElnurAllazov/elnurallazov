2a43542fd82407308dd30ca4810d35df92ddf52f
Elnur Allazov <allazovelnur0102@gmail.com>
2024-10-24 21:21:54 +0400

    g

---
+++ src/main/kotlin/com/example/elnur123/Elnur123Application.kt
@@ -0,0 +1,11 @@
+package com.example.elnur123
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Elnur123Application
+
+fun main(args: Array<String>) {
+    runApplication<Elnur123Application>(*args)
+}
